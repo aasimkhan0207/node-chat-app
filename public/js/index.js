82387869fd3a144ef8320c0f9c1a411a36fc4aba
aasimkhan0207 <aasimkhan0207@gmail.com>
2018-07-12 17:51:06 +0530

    added broadcast feature

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,16 @@ var socket = io();
 
 socket.on('connect',function() {
     console.log('client connected server');
-
     // socket.emit('createMessage',{
     //     from:'aasim@gmail.com',
     //     text:'i have completed ur task'
     // });
-
+    // // u can also emit from dev console eg. socket.emit('createMessage',{from:'robot',text:'hello'});
 });
 
 // custom event newEmail
 socket.on('newMessage', function(message) {
-    console.log('new message ',message);
+    console.log('new message : ',message);
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -11,11 +11,8 @@ app.use(express.static(publicPath));
 var server = http.createServer(app);
 var io = socketIO(server); // web socket server
 
-
 io.on('connection', (socket)=>{
-    // log message when connected
     console.log('New user connected');
-
     // //server to client
     // socket.emit('newMessage',{
     //     from: 'John',
@@ -23,15 +20,34 @@ io.on('connection', (socket)=>{
     //     createdAt:123
     // });// this data will be sent from server to client
 
-    // client to server
+    // TASK 1 : send msg "WELCOME TO CHAT APP" from admin
+    socket.emit('newMessage',{
+        from : "Admin",
+        text : "WELCOME TO CHAT APP"
+    });
+
+    // TASK 2: send "NEW USER JOINED" from admin to everyone else
+    socket.broadcast.emit('newMessage',{
+        from : "Admin",
+        text : "New user joined"
+    });
+
+    // client to server (client send message)
     socket.on('createMessage',(message)=>{
+        // send message to all connections
         io.emit('newMessage',{
             from : message.from,
             text : message.text,
             createdAt : new Date().getTime()
         });
-    });
 
+        // // to everyone else
+        // socket.broadcast.emit('newMessage',{
+        //     from : message.from,
+        //     text : message.text,
+        //     createdAt : new Date().getTime()
+        // });
+    });
 
     socket.on('disconnect',(socket) =>{
         console.log('client Disconnected');
